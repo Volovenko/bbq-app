6e46d903b4af37394f1081333e72bc7003874aad
Volovenko <vadim.volovenko@gmail.com>
2020-03-04 10:32:13 +0300

    fix ruby style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,12 @@ module ApplicationHelper
     asset_path('user.png')
   end
 
-  def flash_class(level)
+  def flash_css_class(level)
     case level
-      when 'info' then "alert alert-info"
-      when 'notice','success' then "alert alert-success"
-      when 'error' then "alert alert-danger"
-      when 'alert' then "alert alert-warning"
+    when 'info' then "alert alert-info"
+    when 'notice','success' then "alert alert-success"
+    when 'error' then "alert alert-danger"
+    when 'alert' then "alert alert-warning"
     end
   end
 end
