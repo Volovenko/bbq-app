6b8f3952b6d3b4f7ad03bd4ff0d8d84fffc7ab7b
Vadim <vadim@MacBook-Air-Vadim.local>
2020-03-11 23:37:08 +0300

    event owner can't subscribe on his own event, add some locales and css, users can't subscribe exists emails

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,19 +1,20 @@
-class Subscription < ApplicationRecord
-  belongs_to :user, optional: true
+class Subscription < ActiveRecord::Base
   belongs_to :event
-  
-  validates :event, presence: true
-  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+  belongs_to :user, optional: true
 
+  validates :event, presence: true
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
-  
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+  validate :check_user_email, unless: -> { user.present? }
+  validate :possibility_user_subscribe, if: -> { user.present? }
+
   def user_name
     if user.present?
-     user.name
+      user.name
     else
-     super
+      super
     end
   end
 
@@ -22,6 +23,20 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  private
+
+  def check_user_email
+    if User.find_by(email: user_email)
+      errors.add(:user_email, I18n.t('errors.check_user_email'))
+    end
+  end
+
+  def possibility_user_subscribe
+    if event.user == user
+      errors.add(:user, I18n.t('errors.possibility_user_subscribe'))
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
 
+  has_many :events, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
   has_many :events
-  has_many :comments
-  has_many :subscriptions
-  
-  before_validation :set_name, on: :create
 
   validates :name, presence: true, length: {maximum: 35}
-  validates :email, presence: true, length: {maximum: 255}
+  validates :email, length: {maximum: 255}
+  validates :email, uniqueness: true
+  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+
+  before_validation :set_name, on: :create
 
   after_commit :link_subscriptions, on: :create
 
@@ -21,6 +24,6 @@ class User < ApplicationRecord
 
   def link_subscriptions
     Subscription.where(user_id: nil, user_email: self.email)
-    .update_all(user_id: self.id)
+      .update_all(user_id: self.id)
   end
 end
